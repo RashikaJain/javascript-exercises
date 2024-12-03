8f904fdb0586e56788515f6733cb8bff1e5626fa
Rashika Jain <jainrashika174@gmail.com>
2024-12-03 23:34:49 +0530

    Add answer to Ques07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  const tempInCelcius = (tempInFahrenheit - 32) * 5/9 ; 
+  const roundOffCelciusValue = (Math.round(tempInCelcius*10))/10 ;
+  return roundOffCelciusValue ; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+  const tempInFahrenheit = ((tempInCelcius*9)/5) + 32 ; 
+  const roundOffFahrenhietValue = (Math.round(tempInFahrenheit*10))/10 ; 
+  return roundOffFahrenhietValue ; 
 };
 
 // Do not edit below this line
