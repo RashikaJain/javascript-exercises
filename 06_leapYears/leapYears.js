8f48012a6a967b18aed38b36d0127c21bf6754d9
Rashika Jain <jainrashika174@gmail.com>
2024-12-03 23:20:05 +0530

    Add answer to ques6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+    const isYearDivisibleByFour = year % 4 == 0  ;
+    const isYearDivisibleBy400 = year%400 == 0 ; 
+    const isYearDivisibleBy100 = year %100 == 0 ;
+    if(isYearDivisibleByFour && 
+    (isYearDivisibleBy400 || !isYearDivisibleBy100))
+        {
+            return true ; 
+        } 
+    else
+    {
+        return false ; 
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
