a2023ed68e0a21d1f2be02e1d335c873c54183d9
Rashika Jain <jainrashika174@gmail.com>
2024-12-04 00:12:09 +0530

    Add answer to question8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,57 @@
-const add = function() {
-	
+const add = function(...valuesToAdd) {
+  let sum = 0 ; 
+  for(let x of valuesToAdd)
+  {
+    sum += x ; 
+  }
+  return sum ; 
 };
 
-const subtract = function() {
-	
+const subtract = function(...values) {
+  let sub ;
+  for(let x of values)
+  {
+    if(values.indexOf(x)==0)
+    {
+      sub=x ; 
+    }
+    else
+    {
+      sub -= x ; 
+    }
+  }
+  return sub ; 
 };
 
-const sum = function() {
-	
+const sum = function(arrayElements) {
+  let totalSum = 0 ; 
+  for(let x of arrayElements.values())
+  {
+    totalSum+= x ; 
+  }
+  return totalSum; 
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multiplication = 1 ; 
+  for(x of arr)
+  {
+    multiplication *= x ; 
+  }
+  return multiplication ; 
 };
 
-const power = function() {
-	
+const power = function(value , powerForValue) {
+  return Math.pow(value , powerForValue) ; 
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+  let factorialOfValue = 1 ; 
+  for(let i = value ; i >0 ;  i--)
+  {
+    factorialOfValue *= i ; 
+  }
+  return factorialOfValue ; 
 };
 
 // Do not edit below this line
