caa5581d2a3cdb46ffb00544de36e9bb8eedca98
Rashika Jain <jainrashika174@gmail.com>
2024-12-03 21:48:43 +0530

    Add answer to question 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(value1 , value2) {
+    if( !Number.isInteger(value1) || !Number.isInteger(value2) ||
+     value1 < 0 || value2 < 0 )
+    {
+        return "ERROR" ; 
+    }
+    let maxNumber = Math.max(value1 , value2) ; 
+    let minNumber = Math.min(value1 , value2) ; 
+    
+    let total = 0 ;  
+    for(let i = minNumber ; i<=maxNumber ; i++)
+    {
+        total += i ; 
+    }
+    return total ; 
 };
 
 // Do not edit below this line
