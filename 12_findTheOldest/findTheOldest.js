6bc268ccfa8b3b1d4720d037c7fd9bd38b2d963b
Rashika Jain <jainrashika174@gmail.com>
2024-12-04 12:36:03 +0530

    Add answer to question12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let maxAge = Number.MIN_VALUE ; 
+    let getname=  "none" ; 
+    for(let x of people)
+    {
+        if(!x.yearOfDeath)
+        {
+            x.yearOfDeath = (new Date()).getFullYear() ; 
+        }
+        let diff = x.yearOfDeath - x.yearOfBirth ;  
+        if(diff > maxAge)
+            {
+                maxAge = diff ;
+                getname = x ; 
+            } 
 
+    }
+    return getname ; 
 };
 
 // Do not edit below this line
