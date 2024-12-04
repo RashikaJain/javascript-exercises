12bf97c36ea8b14bd8dc4e61ec54209e4e4680ab
Rashika Jain <jainrashika174@gmail.com>
2024-12-04 10:38:49 +0530

    Add solution to Question9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (originalString) {
+    let palindromeString = "" ;
+    let originalStringToArray = originalString.split("") ;  // Filter applies only on arrays
+    let specialSymbols = [" ",  "," ,"!" , "&" , "$" , "%" , "}" , "{" ,"|" , "\\" , "~" , "`" , "."] ; 
+    let removeSymbols = originalStringToArray.filter(elem => {return !specialSymbols.includes(elem)});  
+    let joinedString = removeSymbols.join("") ; 
+    for(let x= joinedString.length-1; x>=0  ; x--)
+    {
+        palindromeString += joinedString[x] ; 
+    }
+    return (palindromeString.toLowerCase() === joinedString.toLowerCase()); 
 };
 
 // Do not edit below this line
