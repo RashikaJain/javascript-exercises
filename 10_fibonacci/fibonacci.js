0cac255f9665aa807ded105d78c02d00c811fcf0
Rashika Jain <jainrashika174@gmail.com>
2024-12-04 10:52:23 +0530

    Add answer to Question10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibo) {
+    if(fibo < 0)
+    {
+        return "OOPS" ; 
+    }
+    if(fibo == 0)
+    {
+        return 0 ; 
+    }
+    if(fibo == 1 || fibo== 2)
+    {
+        return 1 ; 
+    }
+    return fibonacci(fibo-1) + fibonacci(fibo - 2) ; 
 };
 
 // Do not edit below this line
