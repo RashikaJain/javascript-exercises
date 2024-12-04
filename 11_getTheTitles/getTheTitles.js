c40fcbd80ffa4e5296a7192ffa8ceaa59d239547
Rashika Jain <jainrashika174@gmail.com>
2024-12-04 10:56:37 +0530

    Add answer to Question11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titleArray = [] ; 
+    for(let x of array)
+    {
+        titleArray.push(x.title) ; 
+    }
+    return titleArray ; 
 };
 
 // Do not edit below this line
